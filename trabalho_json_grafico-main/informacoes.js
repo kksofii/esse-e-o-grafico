4d96a821043ba3efa728f904a7ac690991107ba5
kksofii <126670569+kksofii@users.noreply.github.com>
2024-10-16 09:51:45 -0300

    informacoes.js

--- trabalho_json_grafico-main/informacoes.js
+++ trabalho_json_grafico-main/informacoes.js
@@ -3,15 +3,10 @@ const url = 'https://raw.githubusercontent.com/Akiovictor/trabalho_json_grafico/
 async function vizualizarInformacoes() {
     const res = await fetch(url)
     const dados = await res.json()
-    const quantidade_alunos = (dados.numero_alunos)
-    const numero_votos = (dados.total_votos)
-    const numero_aula_Victor = (dados.total_aula_profVictor)
-    const numero_aula_Mazza = (dados.total_aula_semana)
-    const porcentagem_Victor = ((numero_aula_Victor / numero_aula_Mazza ) * 100)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o <span>3°C</span> tem <span>${quantidade_alunos} alunos</span> e apenas <span>${numero_votos} alunos</span> participaram do questionário? O professpr <span>Victor Yanaguisawa</span> leciona <span>${numero_aula_Victor} aulas por semana</span>, e no colégio há <span>${numero_aula_Mazza} aulas por semana</span>. <br>Isso significa que <span>${porcentagem_Victor}%</span> das aulas acontece com um único professor. Abaixo está o resultado do Questionário`
+    paragrafo.innerHTML = `De acordo com <span>Izabel Marri,</span> gerente de Estudos e Análises Demográficas do IBGE, <span> “temos observado o aumento desse indicador há algum tempo, </span>com ganhos de anos de vida em todas as idades, principalmente devido aos avanços da medicina”</span>`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
